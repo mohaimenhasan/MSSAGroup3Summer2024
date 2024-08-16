db41761d7c6655169494f45dfe91de184a83d3ae
Mohaimen Khan <mohaimenkhan@microsoft.com>
2024-08-16 10:06:44 -0700

    mob3-

--- MSSAGroup3Summer2024/Program.cs
+++ MSSAGroup3Summer2024/Program.cs
@@ -20,43 +20,86 @@
 // 
 
 // what is the input? - an array of integers
-
 // what if the array is empty => return -1 
-
 // what if all of the numbers repeat  => return -1
-
 // is there a time complexity restraint
-
 // all numbers in the array are positive
 
+using System.Linq;
+
 namespace MSSAGroupThree
-
 {
-
     public class Program
-
     {
-
-        public static int FindFirstNonRepeating(List<int> integers)
-
+        public static int FindEasier(List<int> listOfNumbers)
         {
+            if (listOfNumbers.Count == 0)
+            {
+                return -1;
+            }
 
+            HashSet<int> duplicatedNumbers = new();
+            int minIndex = -1;
+
+            for (int i = 0; i < listOfNumbers.Count; i++)
+            {
+                if (duplicatedNumbers.Contains(listOfNumbers[i]))
+                {
+                    continue;
+                }
+                if (listOfNumbers.IndexOf(listOfNumbers[i], i + 1) != -1)
+                {
+                    duplicatedNumbers.Add(listOfNumbers[i]);
+                    continue;
+                }
+                if (minIndex == -1 || listOfNumbers[i] < listOfNumbers[minIndex])
+                {
+                    minIndex = i;
+                }
+            }
+
+            return minIndex;
+        }
+
+
+        public static int FindFirstNonRepeating(List<int> listOfNumbers)
+        {
+            if (listOfNumbers.Count == 0)
+            {
+                return -1;
+            }
+
+            // Michael, Sebastian - Dictionary<int, (int Count, int Index)> keyValuePairs = new Dictionary<int, (int, int)>();
+            // Florabel, Grant, Owen - List
+            Dictionary<int, (int Count, int Index)> keyValuePairs = new Dictionary<int, (int, int)>();
+            foreach (var number in listOfNumbers)
+            {
+                // have a way to check if it's already seen
+                // dictionary
+                if (keyValuePairs.ContainsKey(number))
+                {
+                    var (count, index) = keyValuePairs[number];
+                    keyValuePairs[number] = (count + 1, index);
+                }
+                else
+                {
+                    keyValuePairs.Add(number, (1, listOfNumbers.IndexOf(number)));
+                }
+            }
+
+            // search for the first item that has the count of 1
+            // if it doesn't exist, return -1
+            // if it does exist, return the index
+            var firstNonRepeatingNumber = keyValuePairs.FirstOrDefault(kvp => kvp.Value.Count == 1);
             return -1;
-
         }
 
         static void Main(string[] args)
-
         {
-
-            List<int> integers = new List<int>();
+            List<int> integers = new List<int>() { };
 
             // TODO: add test numbers to integers
-
             FindFirstNonRepeating(integers);
-
         }
-
     }
-
 }
