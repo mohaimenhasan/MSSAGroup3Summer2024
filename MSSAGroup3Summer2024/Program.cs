57d3d9365a8122f099faf550b48a6bef541956fb
Mohaimen Khan <mohaimenkhan@microsoft.com>
2024-08-16 09:32:10 -0700

    mob commit -1

--- MSSAGroup3Summer2024/Program.cs
+++ MSSAGroup3Summer2024/Program.cs
@@ -1,2 +1,17 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+
+// break the question -
+// given the question, what is the purpose of the question
+//
+// 
+// Given an array of integers, write a method that returns the first
+// non-repeating element in the array. 
+
+
+// what questions do we want to ask?
+// what is non-repeating? - an integer that only appears once in the array
+// [5, 6, 7, 2, 1, 3, 5, 6, 7] ==> 2
+// 
+// what is the input? - an array of integers
